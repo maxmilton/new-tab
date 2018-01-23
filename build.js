4cc5d0bb4773a9322d4809ef19286298311dbaca
Max Milton <max@wearegenki.com>
2018-01-24 01:09:47 +1100

    Now builds and runs in browser correctly

--- build.js
+++ build.js
@@ -1,3 +1,5 @@
+// TODO: Minify HTML
+
 'use strict'; // eslint-disable-line
 
 const fs = require('fs');
@@ -8,11 +10,9 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 lasso.configure({
   plugins: ['lasso-marko'],
-  // urlPrefix: '/static',
-  // outputDir: path.join(__dirname, 'dist/static'),
   urlPrefix: '',
   outputDir: path.join(__dirname, 'dist'),
-  bundlingEnabled: true,
+  bundlingEnabled: isProduction,
   minify: isProduction,
   fingerprintsEnabled: false,
   includeSlotNames: false,
@@ -27,19 +27,29 @@ function compile() {
   return new Function('d', 'return `' + template + '`'); // eslint-disable-line
 }
 
-// create JS and CSS bundles + inject into HTML template
+// create JS and CSS bundles
 lasso.lassoPage({
   name: 'app',
   dependencies: ['require-run: ./src/index'],
 }).then((result) => {
-  const head = result.getHtmlForSlot('head');
-  const body = result.getHtmlForSlot('body');
+  const cssFile = result.getCSSFiles()[0];
+  const jsFile = result.getJavaScriptFiles()[0];
+  const css = fs.readFileSync(cssFile); // contents of app.css
+  const js = result.getJavaScriptUrls()[0]; // "app.js"
 
-  fs.writeFileSync('dist/index.html', compile()({ head, body }), 'utf8');
-}).then(() => {
-  // copy manifest
-  fs.copyFileSync('manifest.json', 'dist/manifest.json');
+  // inject into HTML template
+  fs.writeFileSync('dist/index.html', compile()({ css, js }), 'utf8');
+
+  // clean up leftover CSS file
+  fs.unlinkSync(cssFile);
+
+  // all JS in a single file; append ready function
+  fs.writeFileSync(jsFile, `${fs.readFileSync(jsFile)}\n$_mod.ready();`);
 }).catch((err) => {
   throw err;
 });
 
+// copy manifest
+fs.mkdir('dist', () => {
+  fs.copyFileSync('manifest.json', 'dist/manifest.json');
+});
