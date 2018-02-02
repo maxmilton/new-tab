89d8dec5af9a0167f3e76144051bd1b453291f55
Max Milton <max@wearegenki.com>
2018-02-03 09:55:27 +1100

    Manifest improvements

--- manifest.js
+++ manifest.js
@@ -2,20 +2,27 @@
 
 const pkg = require('./package.json');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   manifest_version: 2,
 
   name: 'New Tab',
   description: 'A minimal and high performance Google Chrome new tab page (NTP) extension.',
   version: pkg.version,
+  version_name: process.env.APP_RELEASE,
+  homepage_url: 'https://github.com/MaxMilton/new-tab',
 
   permissions: [
-    'tabs',
     'bookmarks',
-    'history',
     'chrome://favicon/',
+    'tabs',
   ],
   chrome_url_overrides: {
-    newtab: 'index.html',
+    newtab: 'ntp.html',
   },
+  offline_enabled: true,
+
+  // allow connections from http://localhost during local development
+  ...(isProduction ? {} : { content_security_policy: "script-src 'self' blob: filesystem: chrome-extension-resource: http://localhost:*; object-src 'self'" }),
 };
