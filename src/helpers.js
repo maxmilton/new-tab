b901a1bbf85612e15a61328c23f96b837623835f
Max Milton <max@wearegenki.com>
2018-02-04 18:32:51 +1100

    Clean up debounce function (when minified it's the same)

--- src/helpers.js
+++ src/helpers.js
@@ -1,20 +1,25 @@
 /**
- * Throttle calls to a function.
+ * Delay running a function until X ms have passed since its last call.
  * @see https://github.com/developit/decko/blob/master/src/decko.js
  * @param {Function} fn The function to debounce.
  * @param {Number} delay How long to wait for more function calls before executing the function.
  * @returns {Function}
  */
 function debounce(fn, delay) {
-  let args, context, timer;
+  let args;
+  let context;
+  let timer;
 
-  return function(...a) {
+  return function (...a) { // eslint-disable-line func-names
     args = a;
     context = this;
-    if (!timer) timer = setTimeout(() => {
-      fn.apply(context, args);
-      args = context = timer = null;
-    }, delay);
+
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn.apply(context, args);
+        args = context = timer = null; // eslint-disable-line no-multi-assign
+      }, delay);
+    }
   };
 }
 
