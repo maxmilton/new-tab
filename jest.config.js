5146cc6961a9685c0556f27c041739be60c405e4
Max Milton <max@wearegenki.com>
2018-03-12 12:22:57 +1100

    Testing setup

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+'use strict'; // eslint-disable-line
+
+module.exports = {
+  preset: '@wearegenki/test',
+};
---
+++ test/placeholder.test.js
@@ -0,0 +1,7 @@
+/**
+ * Placeholder until we're ready to write the real tests.
+ */
+
+test('placeholder to pass', () => {
+  expect(2 + 3).toEqual(5);
+});
