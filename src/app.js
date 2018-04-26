a93162108ad0ec1aa68e573e079c20f6c9a3dca7
Max Milton <max@wearegenki.com>
2018-04-26 15:38:58 +1000

    Clean up file

--- src/app.js
+++ src/app.js
@@ -3,10 +3,8 @@ import App from './App.html';
 const target = document.body;
 
 // clear static html content
-// TODO: Try to use hydration instead
 target.innerHTML = '';
 
 export default new App({
   target,
-  // hydrate: true,
 });
