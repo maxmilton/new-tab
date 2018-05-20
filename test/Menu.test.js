c79b7b6fc771d644404e85cd6a1943fe6b8a81f4
Max Milton <max@wearegenki.com>
2018-05-21 09:42:23 +1000

    Add test for menu component

--- test/Menu.test.js
+++ test/Menu.test.js
@@ -2,10 +2,22 @@
 
 const Menu = require('../src/components/Menu.html');
 
-describe('Menu root component', () => {
+describe('Menu component', () => {
   it('renders correctly', () => {
     const target = document.createElement('div');
     new Menu({ target });
     expect(target.innerHTML).toMatchSnapshot();
+  });
+
+  it('opens the settings page', () => {
+    expect.assertions(1);
+    const target = document.createElement('div');
+    new Menu({ target });
+    const link = target.querySelector('a:not([href])'); // XXX: Possibly flaky
+    const spy = jest.spyOn(chrome.runtime, 'openOptionsPage', 'get');
+    link.click();
+    expect(spy).toHaveBeenCalled();
+    spy.mockReset();
+    spy.mockRestore();
   });
 });
