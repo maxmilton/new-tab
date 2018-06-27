a8c8fd64959498ae9e3b4fe98ab2378e0d0b59c1
Max Milton <max@wearegenki.com>
2018-06-27 11:29:09 +1000

    Fix broken test coverage

--- test/errors.test.js
+++ test/errors.test.js
@@ -1,10 +1,19 @@
 'use strict';
 
-// const loaderScript = require('../src/loader.js');
+// const errInit = require('../src/errors-init.js');
 // const errScript = require('../src/errors.js');
+// require('../src/errors-init.js');
+// require('../src/errors.js');
 
 describe('Error tracking script', () => {
   it.skip('should do something', () => {
     // TODO
   });
+
+  // it('loads globals', () => {
+  //   expect.assertions(3);
+  //   expect(window.q).toBeDefined();
+  //   expect(window.l).toBeDefined();
+  //   expect(window.u).toBeDefined();
+  // });
 });
