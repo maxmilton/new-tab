ebb73968e9afd4de5c0c4b15a1fa84ed99e82fde
Max Milton <max@wearegenki.com>
2018-08-09 07:51:01 +1000

    Update test snapshots

--- test/components/Menu.test.js
+++ test/components/Menu.test.js
@@ -13,7 +13,7 @@ describe('Menu component', () => {
     expect.assertions(1);
     const target = document.createElement('div');
     new Menu({ target });
-    const link = target.querySelector('a[href="#"]'); // XXX: Possibly flaky
+    const link = target.querySelector('a[href=""]'); // XXX: Possibly flaky
     const spy = jest.spyOn(chrome.runtime, 'openOptionsPage', 'get');
     link.click();
     expect(spy).toHaveBeenCalled();
