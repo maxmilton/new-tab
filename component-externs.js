c55de58d5aa5bbc5c46f26cd5133bc5b1a684c85
Max Milton <max@wearegenki.com>
2018-09-08 14:11:45 +1000

    Fix lint errors

--- component-externs.js
+++ component-externs.js
@@ -3,7 +3,7 @@
  * @externs
  */
 
-/* eslint-disable strict, no-var, no-unused-vars */
+/* eslint-disable strict, no-var, no-unused-vars, id-length */
 
 // Svelte quoted method references
 var i = () => {};
