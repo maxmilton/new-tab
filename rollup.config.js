60f67f07a3f2d2cba29dfdb3db654124e72059d7
Max Milton <max@wearegenki.com>
2018-09-29 19:41:33 +1000

    Extract rollup plugin into a seperate file

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,8 @@ import preprocessMarkup from '@minna-ui/svelte-preprocess-markup';
 import preprocessStyle from '@minna-ui/svelte-preprocess-style';
 import svelte from 'rollup-plugin-svelte';
 import compiler from '@ampproject/rollup-plugin-closure-compiler';
-import { createFilter } from 'rollup-pluginutils'; // eslint-disable-line import/no-extraneous-dependencies
 import { plugin as analyze } from 'rollup-plugin-analyzer';
-import crass from 'crass';
+import makeHtml from './rollup-plugin-make-html.js';
 import manifest from './manifest.js';
 
 const htmlTemplate = readFileSync(`${__dirname}/src/template.html`, 'utf8');
@@ -39,75 +38,6 @@ const compilerOpts = {
  * @param {Error} err
  */
 function catchErr(err) { if (err) throw err; }
-
-/**
- * Ultra-minimal template engine.
- * @see https://github.com/Drulac/template-literal
- * @param {string} template A HTML template to compile.
- * @returns {Function}
- */
-function compileTemplate(template) {
-  return new Function('d', 'return `' + template + '`'); // eslint-disable-line
-}
-
-/**
- * Generate HTML from a template and write it to disk
- * @param {object} opts
- * @param {string} opts.file File path where to save generated HTML document.
- * @param {string} opts.template HTML document template.
- * @param {string=} opts.title Page title.
- * @param {string|Function} opts.content Page content.
- * @param {Array<string>=} opts.exclude Files to exclude from CSS processing.
- * @param {Array<string>=} opts.include Files to include in CSS processing.
- */
-function makeHtml({
-  file,
-  template,
-  title,
-  content,
-  exclude,
-  include = ['**/*.css', '**/*.postcss', '**/*.pcss'],
-  ...options
-}) {
-  const filter = createFilter(include, exclude);
-  const injectHtml = compileTemplate(template);
-  const styles = {};
-
-  return {
-    name: 'makeHtml',
-    transform(source, id) {
-      if (!filter(id)) return;
-
-      styles[id] = source;
-
-      return ''; // eslint-disable-line consistent-return
-    },
-    async generateBundle() {
-      // combine all stylesheets
-      let css = '';
-      for (const id in styles) { // eslint-disable-line
-        css += styles[id] || '';
-      }
-
-      // minify CSS
-      if (!dev && css.length) {
-        css = crass.parse(css).optimize({ o1: true }).toString();
-      }
-
-      // compile HTML from template
-      let body = typeof content === 'function'
-        ? await content()
-        : content;
-      body = body.replace('%CSS%', css.length ? `<style>${css}</style>` : '');
-
-      writeFile(path.join(__dirname, file), injectHtml({
-        title,
-        content: body,
-        ...options,
-      }).trim(), catchErr);
-    },
-  };
-}
 
 const svelteOpts = {
   dev,
