bd31140cf2a1f9ad0f65796695d228720b95dcd0
Max Milton <max@wearegenki.com>
2019-03-10 19:21:13 +1100

    Add types for OffscreenCanvas

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/interface-name-prefix, @typescript-eslint/indent */
+
 declare module '*.svelte' {
   interface IComponentOptions {
     data?: {
@@ -11,3 +13,42 @@ declare module '*.svelte' {
   }
   export = Component;
 }
+
+interface OffscreenCanvasRenderingContext2D
+  extends CanvasState,
+    CanvasTransform,
+    CanvasCompositing,
+    CanvasImageSmoothing,
+    CanvasFillStrokeStyles,
+    CanvasShadowStyles,
+    CanvasFilters,
+    CanvasRect,
+    CanvasDrawPath,
+    CanvasUserInterface,
+    CanvasText,
+    CanvasDrawImage,
+    CanvasImageData,
+    CanvasPathDrawingStyles,
+    CanvasTextDrawingStyles,
+    CanvasPath {
+  readonly canvas: OffscreenCanvas;
+}
+
+declare var OffscreenCanvasRenderingContext2D: {
+  prototype: OffscreenCanvasRenderingContext2D;
+  new (): OffscreenCanvasRenderingContext2D;
+};
+
+interface OffscreenCanvas extends EventTarget {
+  width: number;
+  height: number;
+  getContext(
+    contextId: '2d',
+    contextAttributes?: CanvasRenderingContext2DSettings,
+  ): OffscreenCanvasRenderingContext2D | null;
+}
+
+declare var OffscreenCanvas: {
+  prototype: OffscreenCanvas;
+  new (width: number, height: number): OffscreenCanvas;
+};
