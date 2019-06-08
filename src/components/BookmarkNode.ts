abf99aeec93fb6c82d074d19d6a04c1d5575a864
Max Milton <max@wearegenki.com>
2019-06-08 11:49:36 +1000

    Fix should be a function

--- src/components/BookmarkNode.ts
+++ src/components/BookmarkNode.ts
@@ -2,8 +2,11 @@ import { SvelteComponent } from 'svelte/internal'; // eslint-disable-line import
 import LinkItem from './LinkItem.svelte';
 import BookmarkFolder from './BookmarkFolder.svelte';
 
-export default (context: ComponentOptions): SvelteComponent =>
+export default function BookmarkNode(
+  context: ComponentOptions,
+): SvelteComponent {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  new (context.props!.node.children !== undefined ? BookmarkFolder : LinkItem)(
-    context,
-  );
+  return new (context.props!.node.children !== undefined
+    ? BookmarkFolder
+    : LinkItem)(context);
+}
