dc693c250dbb3e76fb818b0cb89054f6447b15ce
Max Milton <max@wearegenki.com>
2019-06-08 10:29:15 +1000

    Clean up and improve global types

--- global.ts
+++ global.ts
@@ -1,55 +1,18 @@
-/* eslint-disable @typescript-eslint/interface-name-prefix, @typescript-eslint/indent */
-
-interface IComponentOptions {
+// TODO: Use native Svelte component types once they're available
+// @ts-ignore - It's OK for this to not be a module (`--isolatedModules`)
+interface ComponentOptions {
   props?: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [key: string]: any;
+    [key: string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
   };
   target?: Element;
 }
 
 declare module '*.svelte' {
-  class Component {
-    public constructor(options?: IComponentOptions);
+  import { SvelteComponentDev } from 'svelte/internal';
+
+  class Component extends SvelteComponentDev {
+    public constructor(options?: ComponentOptions);
   }
+
   export = Component;
 }
-
-interface OffscreenCanvasRenderingContext2D
-  extends CanvasState,
-    CanvasTransform,
-    CanvasCompositing,
-    CanvasImageSmoothing,
-    CanvasFillStrokeStyles,
-    CanvasShadowStyles,
-    CanvasFilters,
-    CanvasRect,
-    CanvasDrawPath,
-    CanvasUserInterface,
-    CanvasText,
-    CanvasDrawImage,
-    CanvasImageData,
-    CanvasPathDrawingStyles,
-    CanvasTextDrawingStyles,
-    CanvasPath {
-  readonly canvas: OffscreenCanvas;
-}
-
-declare var OffscreenCanvasRenderingContext2D: {
-  prototype: OffscreenCanvasRenderingContext2D;
-  new (): OffscreenCanvasRenderingContext2D;
-};
-
-interface OffscreenCanvas extends EventTarget {
-  width: number;
-  height: number;
-  getContext(
-    contextId: '2d',
-    contextAttributes?: CanvasRenderingContext2DSettings,
-  ): OffscreenCanvasRenderingContext2D | null;
-}
-
-declare var OffscreenCanvas: {
-  prototype: OffscreenCanvas;
-  new (width: number, height: number): OffscreenCanvas;
-};
