e6e533fb2482cec5809bd6d4a9a560a15350bf36
Max Milton <max@wearegenki.com>
2021-03-08 09:18:24 +1000

    Add basic linting back in

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+'use strict'; // eslint-disable-line
+
+const OFF = 0;
+const WARN = 1;
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    '@typescript-eslint/no-use-before-define': WARN,
+    'import/prefer-default-export': OFF,
+    // stage0 uses underscores in synthetic event handler names
+    'no-underscore-dangle': OFF,
+  },
+};
