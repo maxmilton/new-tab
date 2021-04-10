8e04637d3589233d5f558833c4452df36aba0f66
Max Milton <max@wearegenki.com>
2021-04-10 15:31:06 +1000

    Rename vars for better clarity

--- src/components/SearchResult.ts
+++ src/components/SearchResult.ts
@@ -3,7 +3,7 @@ import { Link, LinkProps } from './Link';
 import { TabLink } from './TabLink';
 
 interface SearchResultComponent extends HTMLDivElement {
-  update: (this: void, data: any[]) => void;
+  update: (this: void, newData: any[]) => void;
   filter: (text: string) => void;
 }
 
@@ -28,18 +28,17 @@ const view = h`
 
 export function SearchResult<T extends LinkProps>(
   sectionName: string,
-  raw: T[],
+  data: T[],
 ): SearchResultComponent {
   const root = view.cloneNode(true) as SearchResultComponent;
   const { name, list, more } = view.collect(root) as RefNodes;
   const isOpenTabs = sectionName === 'Open Tabs';
 
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  let _raw: T[];
+  let rawData: T[];
   let renderedLength: number;
 
-  const renderList = (data: T[], showCount = DEFAULT_RESULTS_COUNT) => {
-    const partial = isOpenTabs ? data : data.slice(0, showCount);
+  const renderList = (listData: T[], showCount = DEFAULT_RESULTS_COUNT) => {
+    const partial = isOpenTabs ? listData : listData.slice(0, showCount);
     renderedLength = partial.length;
 
     // Remove all child nodes
@@ -51,28 +50,28 @@ export function SearchResult<T extends LinkProps>(
     }
 
     root.style.display = renderedLength ? 'block' : 'none';
-    more.style.display = isOpenTabs || renderedLength >= data.length ? 'none' : 'block';
+    more.style.display = isOpenTabs || renderedLength >= listData.length ? 'none' : 'block';
   };
 
-  const update = (newRaw: T[]) => {
-    renderList(newRaw);
+  const update = (newData: T[]) => {
+    renderList(newData);
 
-    _raw = newRaw;
+    rawData = newData;
   };
 
   root.update = update;
 
   root.filter = (text) => renderList(
-    _raw.filter(
+    rawData.filter(
       ({ title, url }) => `${url}/${title}`.toLowerCase().indexOf(text.toLowerCase()) > -1,
     ),
   );
 
-  more.__click = () => renderList(_raw, renderedLength + MORE_RESULTS_COUNT);
+  more.__click = () => renderList(rawData, renderedLength + MORE_RESULTS_COUNT);
 
   name.textContent = sectionName;
 
-  update(raw);
+  update(data);
 
   return root;
 }
