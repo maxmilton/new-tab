686585a57b06b131f68d0f49c31f3a71f64c6456
Max Milton <max@wearegenki.com>
2021-05-09 21:36:29 +1000

    Implement test utils

---
+++ test/e2e.ts
@@ -0,0 +1,3 @@
+// TODO: Install and set up `playwright-chromium` -- does it work with extensions?
+//  ↳ REF: https://github.com/bricss/dopant/blob/master/test/fixtures/index.mjs
+export {};
--- test/utils.ts
+++ test/utils.ts
@@ -1,52 +1,93 @@
-import { JSDOM, VirtualConsole } from 'jsdom';
+// FIXME: This file doesn't get included in coverage reports even with `c8 --include=test/utils.ts`
+//  ↳ https://github.com/bcoe/c8/issues/250
 
-const oldGlobal = global;
-const oldConsole = console;
-// const mountedContainers = new Set();
+import { JSDOM } from 'jsdom';
 
-export function setup(): void {
-  const dom = new JSDOM('<!DOCTYPE html>', {
-    // pretendToBeVisual: true,
-    // runScripts: 'dangerously',
-    // url: 'http://localhost',
-    // virtualConsole: new VirtualConsole().sendTo(console),
-    virtualConsole: new VirtualConsole().sendTo(oldConsole),
-  });
+const mountedContainers = new Set<{ container: HTMLDivElement }>();
 
-  // @ts-expect-error - Intentionally overriding global
-  // eslint-disable-next-line no-global-assign
-  global = dom.window.document.defaultView;
-  global.global = oldGlobal;
-
-  console.log('SETUP DONE');
-  // console.log(global);
-  // console.log(global.global);
+function innerText(el: HTMLElement) {
+  // eslint-disable-next-line no-param-reassign
+  el = el.cloneNode(true) as HTMLElement;
+  el.querySelectorAll('script,style').forEach((s) => s.remove());
+  return el.textContent;
 }
 
-export function render(component: Element) {
+function mockInnerText() {
+  Object.defineProperty(global.window.HTMLElement.prototype, 'innerText', {
+    get() {
+      return innerText(this);
+    },
+    set(value: string) {
+      (this as HTMLElement).textContent = value;
+    },
+  });
+}
+
+export function setup(): void {
+  if (global.window) {
+    throw new Error(
+      'JSDOM globals already exist, did you forget to run teardown()?',
+    );
+  }
+
+  const dom = new JSDOM('<!DOCTYPE html>', {
+    pretendToBeVisual: true,
+    runScripts: 'dangerously',
+    url: 'http://localhost/',
+  });
+
+  global.window = dom.window.document.defaultView!;
+  global.document = global.window.document;
+
+  // JSDOM doesn't support innerText yet -- https://github.com/jsdom/jsdom/issues/1245
+  mockInnerText();
+}
+
+export function teardown(): void {
+  if (!global.window) {
+    throw new Error('No JSDOM globals exist, did you forget to run setup()?');
+  }
+
+  // https://github.com/jsdom/jsdom#closing-down-a-jsdom
+  global.window.close();
+  // @ts-expect-error - Cleaning up
+  // eslint-disable-next-line no-multi-assign
+  global.window = global.document = undefined;
+}
+
+export interface RenderResult {
+  /** A wrapper DIV which contains your mounted component. */
+  container: HTMLDivElement;
+  /**
+   * A helper to print the HTML structure of the mounted container. The HTML is
+   * prettified and may not accurately represent your actual HTML. It's intended
+   * for debugging tests only and should not be used in any assertions.
+   */
+  debug(): void;
+}
+
+export function render(component: Node): RenderResult {
   const container = document.createElement('div');
 
   container.appendChild(component);
   document.body.appendChild(container);
 
-  // mountedContainers.add({ container });
+  mountedContainers.add({ container });
 
   return {
     container,
     debug() {
-      // console.log(container.outerHTML);
-      oldConsole.log('DEBUG:');
-      oldConsole.log(container.outerHTML);
+      // TODO: Prettify HTML
+      console.log('DEBUG:');
+      console.log(container.innerHTML);
     },
   };
 }
 
 export function cleanup(): void {
-  document.body.innerText = '';
-
-  // mountedContainers.forEach(({ container }) => {
-  //   if (container.parentNode === document.body) {
-  //     document.body.removeChild(container);
-  //   }
-  // });
+  mountedContainers.forEach(({ container }) => {
+    if (container.parentNode === document.body) {
+      document.body.removeChild(container);
+    }
+  });
 }
