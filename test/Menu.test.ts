9a252836667a0d8ea1507f6d51768261e64bdffa
Max Milton <max@wearegenki.com>
2021-05-11 08:15:44 +1000

    Update test snapshot

--- test/Menu.test.ts
+++ test/Menu.test.ts
@@ -18,25 +18,22 @@ test('renders correctly', () => {
   assert.fixture(
     rendered.container.innerHTML,
     `<div id="menu">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon">
-      <line x1="3" y1="12" x2="21" y2="12"></line>
-      <line x1="3" y1="6" x2="21" y2="6"></line>
-      <line x1="3" y1="18" x2="21" y2="18"></line>
-    </svg>
-
-    <div id="dropdown">
-      <a href="chrome-search://local-ntp/local-ntp.html">Open Default Tab</a>
-      <a href="chrome://bookmarks/">Bookmarks Manager</a>
-      <a href="chrome://downloads/">Downloads</a>
-      <a href="chrome://history/">History</a>
-      <a href="chrome://settings/passwords">Passwords</a>
-
-      <hr>
-
-      <a>New Tab Settings</a>
-      <a href="https://github.com/MaxMilton/new-tab/issues">Submit Bug</a>
-    </div>
-  </div>`,
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon">
+<line x1="3" y1="12" x2="21" y2="12"></line>
+<line x1="3" y1="6" x2="21" y2="6"></line>
+<line x1="3" y1="18" x2="21" y2="18"></line>
+</svg>
+<div id="dropdown">
+<a href="chrome-search://local-ntp/local-ntp.html">Open Default Tab</a>
+<a href="chrome://bookmarks/">Bookmarks Manager</a>
+<a href="chrome://downloads/">Downloads</a>
+<a href="chrome://history/">History</a>
+<a href="chrome://settings/passwords">Passwords</a>
+<hr>
+<a>New Tab Settings</a>
+<a href="https://github.com/MaxMilton/new-tab/issues">Submit Bug</a>
+</div>
+</div>`,
   );
 });
 
