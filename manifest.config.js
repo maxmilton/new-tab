389e22773d08b75140d5e3711479442bdf441017
Max Milton <max@wearegenki.com>
2021-05-11 21:13:36 +1000

    Add registered extension ID to manifest

--- manifest.config.js
+++ manifest.config.js
@@ -7,6 +7,9 @@ const pkg = require('./package.json');
 const manifest = {
   manifest_version: 2,
 
+  // registered extension ID: cpcibnbdmpmcmnkhoiilpnlaepkepknb
+  key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk9BfRa5CXuCX1ElY0yu9kJSqxFirFtSy79ZR/fyKHdOzZurQXNmhIyxVnQXd2bxHvuKUyZGahm/gwgyyzGuxhsQEue6wTD9TnOvvM2vusXpnoCr6Ili7sBwUo9vA2aPI77NB0eArXz9WWNmoDWW5WEqI/rk26Tinl8SNU9iDJISbL+dMses1QPw64oYFWB1J4JeB1MhXnzTxECpGZTn33LhgBU4J3ooT6eoqrsJdRvuc0vjPMxq/jfqLkdBbzlsnrMbgtDoJ9WiWj2lA0MzHGDAQ8HgnMEi3SpXRNnod9CCBnxmkHqv3u4u7Tvp/WLAgJ+QjCt+9yYyw3nOYHpEweQIDAQAB',
+
   name: 'New Tab',
   description:
     '⚡ A high performance new tab page that gets you where you need to go faster.',
