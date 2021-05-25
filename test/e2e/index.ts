61e6a8e671733a6c07e3850519ae440d77507d4a
Max Milton <max@wearegenki.com>
2021-05-25 19:04:20 +1000

    Migrate from yarn1 to pnpm + simplify e2e

--- test/e2e/index.ts
+++ test/e2e/index.ts
@@ -1,4 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies, no-console */
+/* eslint-disable no-console */
 
 // TODO: Write tests to verify:
 //  - No console logs/errors -- including/especially CSP violations
