b97d5f1fc07ebe94350a019694d4c0ed63c937dc
Max Milton <max@wearegenki.com>
2021-06-17 13:22:24 +1000

    Fix lint issues

--- manifest.config.js
+++ manifest.config.js
@@ -37,12 +37,12 @@ const manifest = {
   offline_enabled: true,
   incognito: 'not_allowed',
   content_security_policy:
-    "default-src 'none';" +
+    "default-src 'none';"
     // SHA of theme loader script embedded inline in the document
-    " script-src 'self' 'sha256-FtIFk1UjzWWlhfMrIdJ9n60rbnTmldIAss9HFJTJeUM=';" +
-    " style-src 'unsafe-inline';" +
+    + " script-src 'self' 'sha256-FtIFk1UjzWWlhfMrIdJ9n60rbnTmldIAss9HFJTJeUM=';"
+    + " style-src 'unsafe-inline';"
     // `data:` used in settings page
-    ' img-src chrome://favicon data:;',
+    + ' img-src chrome://favicon data:;',
 
   // https://chrome.google.com/webstore/detail/new-tab/cpcibnbdmpmcmnkhoiilpnlaepkepknb
   key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk9BfRa5CXuCX1ElY0yu9kJSqxFirFtSy79ZR/fyKHdOzZurQXNmhIyxVnQXd2bxHvuKUyZGahm/gwgyyzGuxhsQEue6wTD9TnOvvM2vusXpnoCr6Ili7sBwUo9vA2aPI77NB0eArXz9WWNmoDWW5WEqI/rk26Tinl8SNU9iDJISbL+dMses1QPw64oYFWB1J4JeB1MhXnzTxECpGZTn33LhgBU4J3ooT6eoqrsJdRvuc0vjPMxq/jfqLkdBbzlsnrMbgtDoJ9WiWj2lA0MzHGDAQ8HgnMEi3SpXRNnod9CCBnxmkHqv3u4u7Tvp/WLAgJ+QjCt+9yYyw3nOYHpEweQIDAQAB',
