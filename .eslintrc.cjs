2d093903dc3c881140237b146fc5e7bad2d5cce6
Max Milton <max@wearegenki.com>
2021-06-27 19:34:45 +1000

    Configure lint tooling

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,14 +17,35 @@ module.exports = {
     'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:unicorn/recommended',
   ],
   rules: {
     '@typescript-eslint/no-use-before-define': WARN,
     'import/extensions': WARN,
     'import/prefer-default-export': OFF,
+    // byte savings with same performance
+    'prefer-template': OFF,
+    // byte savings
+    'no-plusplus': OFF,
     'no-restricted-syntax': OFF,
     // stage1 uses underscores in synthetic event handler names
     'no-underscore-dangle': OFF,
     'no-void': OFF,
+    // byte savings + faster
+    'unicorn/explicit-length-check': OFF,
+    'unicorn/filename-case': OFF,
+    'unicorn/no-abusive-eslint-disable': WARN,
+    'unicorn/no-array-callback-reference': OFF,
+    // array forEach is actually faster now!
+    'unicorn/no-array-for-each': OFF,
+    'unicorn/no-null': OFF,
+    'unicorn/prefer-add-event-listener': OFF,
+    'unicorn/prefer-dom-node-append': OFF,
+    'unicorn/prefer-module': OFF,
+    'unicorn/prefer-node-protocol': OFF,
+    // indexOf is faster (in Chrome)
+    'unicorn/prefer-includes': OFF,
+    'unicorn/prefer-query-selector': OFF,
+    'unicorn/prevent-abbreviations': OFF,
   },
 };
