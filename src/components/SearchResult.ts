3dcbb379cd7b8f6d5790f4c848eab38b3502babf
Max Milton <max@wearegenki.com>
2021-06-27 19:41:33 +1000

    Fix search result list rendering performance + improve SearchResult component

--- src/components/SearchResult.ts
+++ src/components/SearchResult.ts
@@ -1,5 +1,5 @@
 import { h, S1Node } from 'stage1';
-import { append } from '../utils';
+import { append, createFragment } from '../utils';
 import { Link, LinkProps } from './Link';
 import { TabLink } from './TabLink';
 
@@ -9,21 +9,21 @@ HTMLDivElement & {
   filter: (text: string) => void;
 };
 type RefNodes = {
-  name: Text;
-  list: HTMLDivElement;
-  more: HTMLButtonElement;
+  t: Text;
+  l: HTMLDivElement;
+  m: HTMLButtonElement;
 };
 
-const DEFAULT_RESULTS_COUNT = 10;
-const MORE_RESULTS_COUNT = 50;
+const DEFAULT_RESULTS_AMOUNT = 10;
+const MORE_RESULTS_AMOUNT = 50;
 
 const view = h`
   <div>
-    <h2 #name></h2>
+    <h2 #t></h2>
 
-    <div #list></div>
+    <div #l></div>
 
-    <button #more>Show more ▾</button>
+    <button #m>Show more ▾</button>
   </div>
 `;
 
@@ -32,25 +32,28 @@ export function SearchResult<T extends LinkProps>(
   data: T[],
 ): SearchResultComponent {
   const root = view.cloneNode(true) as SearchResultComponent;
-  const { name, list, more } = view.collect<RefNodes>(root);
+  const { t, l, m } = view.collect<RefNodes>(root);
   const isOpenTabs = sectionName === 'Open Tabs';
   let rawData: T[];
   let renderedLength: number;
 
-  const renderList = (listData: T[], showCount = DEFAULT_RESULTS_COUNT) => {
+  const renderList = (listData: T[], showCount = DEFAULT_RESULTS_AMOUNT) => {
     const partial = isOpenTabs ? listData : listData.slice(0, showCount);
+    const frag = createFragment();
     renderedLength = partial.length;
 
-    // Remove all child nodes
-    list.textContent = '';
+    partial.forEach((item) => {
+      // @ts-expect-error - FIXME:!
+      append((isOpenTabs ? TabLink : Link)(item), frag);
+    });
 
-    for (const item of partial) {
-      // @ts-expect-error - FIXME
-      append((isOpenTabs ? TabLink : Link)(item), list);
-    }
+    // remove all child nodes
+    l.textContent = '';
+
+    append(frag, l);
 
     root.hidden = !renderedLength;
-    more.hidden = isOpenTabs || renderedLength >= listData.length;
+    m.hidden = isOpenTabs || renderedLength >= listData.length;
   };
 
   const update = (newData: T[]) => {
@@ -62,13 +65,13 @@ export function SearchResult<T extends LinkProps>(
 
   root.filter = (text) => renderList(
     rawData.filter(
-      ({ title, url }) => `${url}/${title}`.toLowerCase().indexOf(text.toLowerCase()) > -1,
+      ({ title, url }) => (url + '+' + title).toLowerCase().indexOf(text.toLowerCase()) > -1,
     ),
   );
 
-  name.textContent = sectionName;
+  t.textContent = sectionName;
 
-  more.__click = () => renderList(rawData, renderedLength + MORE_RESULTS_COUNT);
+  m.__click = () => renderList(rawData, renderedLength + MORE_RESULTS_AMOUNT);
 
   update(data);
 
