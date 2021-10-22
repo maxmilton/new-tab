85fcb8774841873f32b3eb5646af0a4a0d2910f9
Max Milton <max@wearegenki.com>
2021-10-23 08:41:04 +1000

    Always preload theme regardless of install reason

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,12 @@
 import type { UserStorageData } from './types';
 
-// On first install or subsequent updates load user's theme into localStorage
-chrome.runtime.onInstalled.addListener((details) => {
-  if (details.reason === 'install' || details.reason === 'update') {
-    void fetch('themes.json')
-      .then((res) => res.json())
-      .then((themesData: Record<string, string>) => {
-        chrome.storage.local.get(null, (settings: UserStorageData) => {
-          localStorage.t = themesData[settings.t || 'light'];
-        });
+// On install or any subsequent update load user's theme into localStorage
+chrome.runtime.onInstalled.addListener(() => {
+  void fetch('themes.json')
+    .then((res) => res.json())
+    .then((themesData: Record<string, string>) => {
+      chrome.storage.local.get(null, (settings: UserStorageData) => {
+        localStorage.t = themesData[settings.t || 'light'];
       });
-  }
+    });
 });
