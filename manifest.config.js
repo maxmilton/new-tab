02931ac0dab3fd02bf448fc913cbac96eb9a469e
Max Milton <max@wearegenki.com>
2021-10-24 15:19:15 +1000

    Minor extension manifest tweaks

--- manifest.config.js
+++ manifest.config.js
@@ -14,9 +14,7 @@ const manifest = {
   description:
     '⚡ A high performance new tab page that gets you where you need to go faster.',
   version: pkg.version,
-  version_name: process.env.GITHUB_REF
-    ? process.env.GITHUB_REF.replace('refs/tags/v', '')
-    : gitRef().replace(/^v/, ''),
+  version_name: process.env.GITHUB_REF ? undefined : gitRef().replace(/^v/, ''),
   homepage_url: pkg.homepage,
   icons: {
     16: 'icon16.png',
@@ -47,11 +45,11 @@ const manifest = {
   incognito: 'not_allowed',
   content_security_policy:
     "default-src 'none';"
-    // hash of inline theme loader script embedded in the HTML
-    + " script-src-elem 'self' 'sha256-N5HgwudrK2iZWAvytRoTz8UeLeW+Lc/QQLSVUFL/z/0=';"
-    // app styles are embedded in the HTML for fastest load performance
-    + " style-src-elem 'unsafe-inline';"
-    + ' img-src chrome://favicon;',
+    // Hash of inline theme loader script embedded in the HTML
+    + "script-src-elem 'self' 'sha256-N5HgwudrK2iZWAvytRoTz8UeLeW+Lc/QQLSVUFL/z/0=';"
+    // App styles are embedded in the HTML for fastest load performance
+    + "style-src-elem 'unsafe-inline';"
+    + 'img-src chrome://favicon;',
 
   // https://chrome.google.com/webstore/detail/new-tab/cpcibnbdmpmcmnkhoiilpnlaepkepknb
   key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk9BfRa5CXuCX1ElY0yu9kJSqxFirFtSy79ZR/fyKHdOzZurQXNmhIyxVnQXd2bxHvuKUyZGahm/gwgyyzGuxhsQEue6wTD9TnOvvM2vusXpnoCr6Ili7sBwUo9vA2aPI77NB0eArXz9WWNmoDWW5WEqI/rk26Tinl8SNU9iDJISbL+dMses1QPw64oYFWB1J4JeB1MhXnzTxECpGZTn33LhgBU4J3ooT6eoqrsJdRvuc0vjPMxq/jfqLkdBbzlsnrMbgtDoJ9WiWj2lA0MzHGDAQ8HgnMEi3SpXRNnod9CCBnxmkHqv3u4u7Tvp/WLAgJ+QjCt+9yYyw3nOYHpEweQIDAQAB',
