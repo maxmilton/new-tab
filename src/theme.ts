a38d46986d777f92674db2972539c3ddec89a4e3
Max Milton <max@happycode.ltd>
2022-08-29 07:48:40 +1000

    chore: Remove unnecessary lint ignore comments

--- src/theme.ts
+++ src/theme.ts
@@ -8,10 +8,7 @@ performance.mark('Load Theme');
 
 chrome.storage.local.get('t', (settings: UserStorageData) => {
   const sheet = new CSSStyleSheet();
-  // @ts-expect-error - replaceSync method will be added in TypeScript v4.8
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   sheet.replaceSync(settings.t!);
-  // @ts-expect-error - adoptedStyleSheets method will be added in TypeScript v4.8
   document.adoptedStyleSheets = [sheet];
 
   performance.measure('Load Theme', 'Load Theme');
