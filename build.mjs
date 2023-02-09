217fd0276d283e8a9cba350f5801e7e9839cb44e
Max Milton <max@happyhackers.xyz>
2023-02-09 14:49:48 +1100

    chore: Refactor build + update to new esbuild API
    - Update esbuild package
    - Use new esbuild watch API
    - Reorder execution in build; esbuild now runs last (otherwise watch prevents execution of things after)
    - Fix CSS compile warnings when line or collumn number is 0
    - Increate minimum chrome version target to v110
    - Move themes file generation logic into dedicated function for better code readibility

--- build.mjs
+++ build.mjs
@@ -18,6 +18,102 @@ const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const dir = path.resolve(); // == __dirname
 
+/**
+ * Generate minified CSS from XCSS source.
+ *
+ * @param {string} src
+ * @param {string} from
+ */
+function compileCSS(src, from) {
+  const compiled = xcss.compile(src, {
+    from,
+    map: false,
+  });
+
+  for (const warning of compiled.warnings) {
+    console.error('XCSS WARNING:', warning.message);
+
+    if (warning.file) {
+      console.log(
+        `  at ${[warning.file, warning.line, warning.column]
+          .filter((x) => x != null)
+          .join(':')}`,
+      );
+    }
+  }
+
+  const minified = lightningcss.transform({
+    filename: from,
+    code: Buffer.from(compiled.css),
+    minify: !dev,
+    sourceMap: false,
+    targets: {
+      // eslint-disable-next-line no-bitwise
+      chrome: 110 << 16,
+    },
+  });
+
+  for (const warning of minified.warnings) {
+    console.error('CSS WARNING:', warning.message);
+  }
+
+  return minified.code.toString();
+}
+
+/**
+ * Construct a HTML file and save it to disk.
+ *
+ * @param {string} pageName
+ * @param {string} stylePath
+ */
+async function makeHTML(pageName, stylePath) {
+  const styleSrc = await fs.readFile(path.join(dir, stylePath), 'utf8');
+  const css = compileCSS(styleSrc, stylePath);
+  const template = `<!doctype html>
+<meta charset=utf-8>
+<title>New Tab</title>
+<link href=${pageName}.css rel=stylesheet>
+<script src=${pageName}.js async></script>`;
+
+  await fs.writeFile(path.join(dir, 'dist', `${pageName}.css`), css);
+  await fs.writeFile(path.join(dir, 'dist', `${pageName}.html`), template);
+}
+
+await makeHTML('newtab', 'src/css/newtab.xcss');
+await makeHTML('settings', 'src/css/settings.xcss');
+
+// Extension manifest
+await fs.writeFile(
+  path.join(dir, 'dist', 'manifest.json'),
+  JSON.stringify(manifest),
+);
+
+/**
+ * Compile all themes, combine into a single JSON file, and save it to disk.
+ */
+async function makeThemes() {
+  const themesDir = path.resolve('.', 'src/css/themes');
+  /** @type {Record<string, string>} */
+  const themesData = {};
+  const themes = await fs.readdir(themesDir);
+
+  await Promise.all(
+    themes.map((theme) => fs.readFile(path.join(themesDir, theme), 'utf8').then((src) => {
+      themesData[path.basename(theme, '.xcss')] = compileCSS(src, theme);
+    })),
+  );
+
+  await fs.writeFile(
+    path.join(dir, 'dist', 'themes.json'),
+    JSON.stringify(themesData),
+  );
+}
+
+const t0 = performance.now();
+await makeThemes();
+const t1 = performance.now();
+console.log('\ndist/themes.json done in', (t1 - t0).toFixed(2), 'ms\n');
+
 /** @type {esbuild.Plugin} */
 const analyzeMeta = {
   name: 'analyze-meta',
@@ -60,11 +156,12 @@ const minifyJS = {
 };
 
 // New Tab & Settings apps
-await esbuild.build({
+/** @type {esbuild.BuildOptions} */
+const esbuildOptions1 = {
   entryPoints: ['src/newtab.ts', 'src/settings.ts'],
   outdir: 'dist',
   platform: 'browser',
-  target: ['chrome104'],
+  target: ['chrome110'],
   format: 'esm',
   define: { 'process.env.NODE_ENV': JSON.stringify(mode) },
   plugins: [minifyTemplates(), minifyJS, writeFiles(), analyzeMeta],
@@ -73,16 +170,16 @@ await esbuild.build({
   mangleProps: /_refs|collect|adjustPosition|closePopup/,
   sourcemap: dev,
   banner: { js: '"use strict";' },
-  watch: dev,
   write: dev,
   metafile: !dev && process.stdout.isTTY,
   logLevel: 'debug',
   // XXX: Comment out to keep performance markers in non-dev builds for debugging
   pure: ['performance.mark', 'performance.measure'],
-});
+};
 
 // Background service worker script
-await esbuild.build({
+/** @type {esbuild.BuildOptions} */
+const esbuildOptions2 = {
   entryPoints: ['src/sw.ts'],
   outdir: 'dist',
   format: 'esm',
@@ -91,95 +188,13 @@ await esbuild.build({
   minify: !dev,
   metafile: !dev && process.stdout.isTTY,
   logLevel: 'debug',
-});
+};
 
-/**
- * Generate minified CSS from XCSS source.
- *
- * @param {string} src
- * @param {string} from
- */
-function compileCSS(src, from) {
-  const compiled = xcss.compile(src, {
-    from,
-    map: false,
-  });
-
-  for (const warning of compiled.warnings) {
-    console.error('XCSS WARNING:', warning.message);
-
-    if (warning.file) {
-      console.log(
-        `  at ${[warning.file, warning.line, warning.column]
-          .filter(Boolean)
-          .join(':')}`,
-      );
-    }
-  }
-
-  const minified = lightningcss.transform({
-    filename: from,
-    code: Buffer.from(compiled.css),
-    minify: true,
-    sourceMap: dev,
-    targets: {
-      // eslint-disable-next-line no-bitwise
-      chrome: 104 << 16,
-    },
-  });
-
-  for (const warning of minified.warnings) {
-    console.error('CSS WARNING:', warning.message);
-  }
-
-  return minified.code.toString();
+if (dev) {
+  const context1 = await esbuild.context(esbuildOptions1);
+  const context2 = await esbuild.context(esbuildOptions2);
+  await Promise.all([context1.watch(), context2.watch()]);
+} else {
+  await esbuild.build(esbuildOptions1);
+  await esbuild.build(esbuildOptions2);
 }
-
-/**
- * Construct a HTML file and save it to disk.
- *
- * @param {string} pageName
- * @param {string} stylePath
- */
-async function makeHTML(pageName, stylePath) {
-  const styleSrc = await fs.readFile(path.join(dir, stylePath), 'utf8');
-  const css = compileCSS(styleSrc, stylePath);
-  const template = `<!doctype html>
-<meta charset=utf-8>
-<title>New Tab</title>
-<link href=${pageName}.css rel=stylesheet>
-<script src=${pageName}.js async></script>`;
-
-  await fs.writeFile(path.join(dir, 'dist', `${pageName}.css`), css);
-  await fs.writeFile(path.join(dir, 'dist', `${pageName}.html`), template);
-}
-
-await makeHTML('newtab', 'src/css/newtab.xcss');
-await makeHTML('settings', 'src/css/settings.xcss');
-
-// Extension manifest
-await fs.writeFile(
-  path.join(dir, 'dist', 'manifest.json'),
-  JSON.stringify(manifest),
-);
-
-const t0 = performance.now();
-
-const themesDir = path.resolve('.', 'src/css/themes');
-/** @type {Record<string, string>} */
-const themesData = {};
-const themes = await fs.readdir(themesDir);
-
-await Promise.all(
-  themes.map((theme) => fs.readFile(path.join(themesDir, theme), 'utf8').then((src) => {
-    themesData[path.basename(theme, '.xcss')] = compileCSS(src, theme);
-  })),
-);
-
-await fs.writeFile(
-  path.join(dir, 'dist', 'themes.json'),
-  JSON.stringify(themesData),
-);
-
-const t1 = performance.now();
-console.log('\ndist/themes.json done in', (t1 - t0).toFixed(2), 'ms\n');
