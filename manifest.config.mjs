11d136312a4e00666b6bac3a861a103f98830cdd
Max Milton <max@happyhackers.xyz>
2023-05-15 14:00:43 +1000

    chore: Add debug code for CI

--- manifest.config.mjs
+++ manifest.config.mjs
@@ -6,51 +6,103 @@
 import { gitRef } from 'git-ref';
 import pkg from './package.json' assert { type: 'json' };
 
-/** @type {() => chrome.runtime.Manifest} */
-export const manifest = () => ({
-  manifest_version: 3,
-  name: 'New Tab',
-  description: pkg.description,
-  version: pkg.version,
-  // production releases should not have a named version
-  version_name: process.env.CI ? undefined : gitRef().replace(/^v/, ''),
-  minimum_chrome_version: '113',
-  homepage_url: pkg.homepage,
-  icons: {
-    16: 'icon16.png',
-    48: 'icon48.png',
-    128: 'icon128.png',
-  },
-  permissions: [
-    'bookmarks',
-    'favicon',
-    'history',
-    'sessions',
-    'storage',
-    'tabs',
-    'topSites',
-  ],
-  chrome_url_overrides: {
-    newtab: 'newtab.html',
-  },
-  background: {
-    service_worker: 'sw.js',
-  },
-  options_ui: {
-    page: 'settings.html',
-    // open_in_tab: true, // dev only
-  },
-  offline_enabled: true,
-  incognito: 'not_allowed',
-  content_security_policy: {
-    extension_pages: [
-      "default-src 'none'",
-      "script-src 'self'",
-      "style-src 'self'",
-      "img-src 'self'",
-      "base-uri 'none'",
-    ].join(';'),
-  },
-  // https://chrome.google.com/webstore/detail/new-tab/cpcibnbdmpmcmnkhoiilpnlaepkepknb
-  key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk9BfRa5CXuCX1ElY0yu9kJSqxFirFtSy79ZR/fyKHdOzZurQXNmhIyxVnQXd2bxHvuKUyZGahm/gwgyyzGuxhsQEue6wTD9TnOvvM2vusXpnoCr6Ili7sBwUo9vA2aPI77NB0eArXz9WWNmoDWW5WEqI/rk26Tinl8SNU9iDJISbL+dMses1QPw64oYFWB1J4JeB1MhXnzTxECpGZTn33LhgBU4J3ooT6eoqrsJdRvuc0vjPMxq/jfqLkdBbzlsnrMbgtDoJ9WiWj2lA0MzHGDAQ8HgnMEi3SpXRNnod9CCBnxmkHqv3u4u7Tvp/WLAgJ+QjCt+9yYyw3nOYHpEweQIDAQAB',
-});
+/** @returns {chrome.runtime.Manifest} */
+// export const manifest = () => ({
+//   manifest_version: 3,
+//   name: 'New Tab',
+//   description: pkg.description,
+//   version: pkg.version,
+//   // shippable releases should not have a named version
+//   version_name: process.env.CI ? undefined : gitRef().replace(/^v/, ''),
+//   minimum_chrome_version: '113',
+//   homepage_url: pkg.homepage,
+//   icons: {
+//     16: 'icon16.png',
+//     48: 'icon48.png',
+//     128: 'icon128.png',
+//   },
+//   permissions: [
+//     'bookmarks',
+//     'favicon',
+//     'history',
+//     'sessions',
+//     'storage',
+//     'tabs',
+//     'topSites',
+//   ],
+//   chrome_url_overrides: {
+//     newtab: 'newtab.html',
+//   },
+//   background: {
+//     service_worker: 'sw.js',
+//   },
+//   options_ui: {
+//     page: 'settings.html',
+//     // open_in_tab: true, // dev only
+//   },
+//   offline_enabled: true,
+//   incognito: 'not_allowed',
+//   content_security_policy: {
+//     extension_pages: [
+//       "default-src 'none'",
+//       "script-src 'self'",
+//       "style-src 'self'",
+//       "img-src 'self'",
+//       "base-uri 'none'",
+//     ].join(';'),
+//   },
+//   // https://chrome.google.com/webstore/detail/new-tab/cpcibnbdmpmcmnkhoiilpnlaepkepknb
+//   key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk9BfRa5CXuCX1ElY0yu9kJSqxFirFtSy79ZR/fyKHdOzZurQXNmhIyxVnQXd2bxHvuKUyZGahm/gwgyyzGuxhsQEue6wTD9TnOvvM2vusXpnoCr6Ili7sBwUo9vA2aPI77NB0eArXz9WWNmoDWW5WEqI/rk26Tinl8SNU9iDJISbL+dMses1QPw64oYFWB1J4JeB1MhXnzTxECpGZTn33LhgBU4J3ooT6eoqrsJdRvuc0vjPMxq/jfqLkdBbzlsnrMbgtDoJ9WiWj2lA0MzHGDAQ8HgnMEi3SpXRNnod9CCBnxmkHqv3u4u7Tvp/WLAgJ+QjCt+9yYyw3nOYHpEweQIDAQAB',
+// });
+export const manifest = () => {
+  console.log('@@@@ CI', process.env.CI);
+  console.log('@@@@ gitRef', gitRef());
+
+  return {
+    manifest_version: 3,
+    name: 'New Tab',
+    description: pkg.description,
+    version: pkg.version,
+    // shippable releases should not have a named version
+    version_name: process.env.CI ? undefined : gitRef().replace(/^v/, ''),
+    minimum_chrome_version: '113',
+    homepage_url: pkg.homepage,
+    icons: {
+      16: 'icon16.png',
+      48: 'icon48.png',
+      128: 'icon128.png',
+    },
+    permissions: [
+      'bookmarks',
+      'favicon',
+      'history',
+      'sessions',
+      'storage',
+      'tabs',
+      'topSites',
+    ],
+    chrome_url_overrides: {
+      newtab: 'newtab.html',
+    },
+    background: {
+      service_worker: 'sw.js',
+    },
+    options_ui: {
+      page: 'settings.html',
+      // open_in_tab: true, // dev only
+    },
+    offline_enabled: true,
+    incognito: 'not_allowed',
+    content_security_policy: {
+      extension_pages: [
+        "default-src 'none'",
+        "script-src 'self'",
+        "style-src 'self'",
+        "img-src 'self'",
+        "base-uri 'none'",
+      ].join(';'),
+    },
+    // https://chrome.google.com/webstore/detail/new-tab/cpcibnbdmpmcmnkhoiilpnlaepkepknb
+    key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk9BfRa5CXuCX1ElY0yu9kJSqxFirFtSy79ZR/fyKHdOzZurQXNmhIyxVnQXd2bxHvuKUyZGahm/gwgyyzGuxhsQEue6wTD9TnOvvM2vusXpnoCr6Ili7sBwUo9vA2aPI77NB0eArXz9WWNmoDWW5WEqI/rk26Tinl8SNU9iDJISbL+dMses1QPw64oYFWB1J4JeB1MhXnzTxECpGZTn33LhgBU4J3ooT6eoqrsJdRvuc0vjPMxq/jfqLkdBbzlsnrMbgtDoJ9WiWj2lA0MzHGDAQ8HgnMEi3SpXRNnod9CCBnxmkHqv3u4u7Tvp/WLAgJ+QjCt+9yYyw3nOYHpEweQIDAQAB',
+  };
+};
