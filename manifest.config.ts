c1f3aee790db0e17ff33ebe8cee11091a9f2ebbc
Max Milton <max@happyhackers.xyz>
2023-12-13 12:56:29 +1100

    chore: Add comments to manifest.config for clarity

--- manifest.config.ts
+++ manifest.config.ts
@@ -35,11 +35,11 @@ export const createManifest = (
   manifest_version: 3,
   name: 'New Tab',
   description: pkg.description,
+  homepage_url: pkg.homepage,
   version: pkg.version,
   // shippable releases should not have a named version
   version_name: debug ? gitRef() : undefined,
   minimum_chrome_version: '116', // for new password manager link
-  homepage_url: pkg.homepage,
   icons: {
     16: 'icon16.png',
     48: 'icon48.png',
@@ -47,11 +47,11 @@ export const createManifest = (
   },
   permissions: [
     'bookmarks',
-    'favicon',
+    'favicon', // for favicon cache
     'history',
-    'sessions',
-    'storage',
-    'tabs',
+    'sessions', // for recently closed tabs
+    'storage', // to persist user settings
+    'tabs', // to access tab title and URL
     'topSites',
   ],
   chrome_url_overrides: {
@@ -82,6 +82,7 @@ export const createManifest = (
   cross_origin_opener_policy: {
     value: 'same-origin',
   },
+
   // https://chrome.google.com/webstore/detail/new-tab/cpcibnbdmpmcmnkhoiilpnlaepkepknb
   key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk9BfRa5CXuCX1ElY0yu9kJSqxFirFtSy79ZR/fyKHdOzZurQXNmhIyxVnQXd2bxHvuKUyZGahm/gwgyyzGuxhsQEue6wTD9TnOvvM2vusXpnoCr6Ili7sBwUo9vA2aPI77NB0eArXz9WWNmoDWW5WEqI/rk26Tinl8SNU9iDJISbL+dMses1QPw64oYFWB1J4JeB1MhXnzTxECpGZTn33LhgBU4J3ooT6eoqrsJdRvuc0vjPMxq/jfqLkdBbzlsnrMbgtDoJ9WiWj2lA0MzHGDAQ8HgnMEi3SpXRNnod9CCBnxmkHqv3u4u7Tvp/WLAgJ+QjCt+9yYyw3nOYHpEweQIDAQAB',
 });
