fa9ba08d1f3d46702b4da010e1b262ff61faedce
Max Milton <max@happyhackers.xyz>
2024-07-02 15:21:33 +0900

    feat: Auto reload theme in development

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,7 @@
  * Theme Loader
  */
 
+import type { ThemesData } from './types';
 import { storage } from './utils';
 
 performance.mark('Load Theme');
@@ -12,3 +13,17 @@ sheet.replaceSync(storage.t!);
 document.adoptedStyleSheets = [sheet];
 
 performance.measure('Load Theme', 'Load Theme');
+
+if (process.env.NODE_ENV === 'development') {
+  requestIdleCallback(() => {
+    void fetch('themes.json')
+      .then((res) => res.json())
+      .then((themes: ThemesData) => {
+        const theme = themes[storage.tn ?? 'auto'];
+        if (storage.t !== theme) {
+          sheet.replaceSync(theme);
+          void chrome.storage.local.set({ t: theme });
+        }
+      });
+  });
+}
