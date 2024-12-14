d5c637df525b85ad9358f23f6bb05caec6c22f35
Max Milton <max@happyhackers.xyz>
2024-12-15 00:09:32 +0900

    feat: Add settings sync

--- src/sw.ts
+++ src/sw.ts
@@ -1,13 +1,13 @@
 /// <reference lib="webworker" />
 
-import type { ThemesData, UserStorageData } from './types';
+import type { SyncStorageData, ThemesData, UserStorageData } from './types';
 
 // On install or subsequent update, preload the user's chosen theme into storage
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 chrome.runtime.onInstalled.addListener(async () => {
   const [themes, settings] = await Promise.all([
     fetch('themes.json').then((res) => res.json() as Promise<ThemesData>),
-    chrome.storage.local.get<UserStorageData>(),
+    chrome.storage.local.get<UserStorageData>('tn'),
   ]);
 
   // TODO: Remove once most users have updated.
@@ -29,4 +29,35 @@ chrome.runtime.onInstalled.addListener(async () => {
   //     url: chrome.runtime.getURL('settings.html'),
   //   });
   // }
+});
+
+/* ********************************** */
+// Experimental sync settings feature //
+/* ********************************** */
+
+// On profile startup, pull remote user settings; local <- sync
+chrome.runtime.onStartup.addListener(() => {
+  void chrome.storage.local
+    .get<UserStorageData>(['s', 'tn'])
+    .then((settings) => {
+      // Only when sync is enabled
+      if (!settings.s) return;
+
+      void chrome.storage.sync.get<SyncStorageData>().then((remote) => {
+        if (remote.data) {
+          if (remote.data.tn === settings.tn) {
+            void chrome.storage.local.set(remote.data);
+          } else {
+            void fetch('themes.json')
+              .then((res) => res.json() as Promise<ThemesData>)
+              .then((themes) => {
+                void chrome.storage.local.set({
+                  t: themes[settings.tn ?? 'auto'],
+                  ...remote.data,
+                });
+              });
+          }
+        }
+      });
+    });
 });
