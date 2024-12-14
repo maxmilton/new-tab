3204fada9a251abf20b295564cc52f05d28acdb7
Max Milton <max@happyhackers.xyz>
2024-12-14 13:16:05 +0900

    feat: Use new stage1 fragment API

--- src/newtab.ts
+++ src/newtab.ts
@@ -1,7 +1,7 @@
 // Theme loader code must run first
 import './theme';
 
-import { append, createFragment } from 'stage1';
+import { append, fragment } from 'stage1';
 import { BookmarkBar } from './components/BookmarkBar';
 import { Menu } from './components/Menu';
 import { Search } from './components/Search';
@@ -9,16 +9,16 @@ import { handleClick, storage } from './utils';
 
 performance.mark('Initialise Components');
 
-const frag = createFragment();
+const container = fragment();
 // Create Search component first because it has asynchronous calls that can
 // execute while the remaining components are constructed
-append(Search(), frag);
+append(Search(), container);
 // Create BookmarkBar component near last because, after an async call, it needs
 // to synchronously and sequentially calculate DOM layout multiple times and
 // could cause reflow in extreme situations, so paint the rest of the app first
-if (!storage.b) append(BookmarkBar(), frag);
-append(Menu(), frag);
-append(frag, document.body);
+if (!storage.b) append(BookmarkBar(), container);
+append(Menu(), container);
+append(container, document.body);
 
 performance.measure('Initialise Components', 'Initialise Components');
 
