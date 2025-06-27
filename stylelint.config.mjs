489d7f6ebb8135155145348ca3b4badac95c030c
Max Milton <max@happyhackers.xyz>
2025-06-27 14:06:07 +0900

    chore: Adjust Stylelint config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,7 +3,7 @@ export default {
   reportInvalidScopeDisables: true,
   reportNeedlessDisables: true,
   extends: ['stylelint-config-standard', '@maxmilton/stylelint-config'],
-  ignoreFiles: ['**/*.bak/**', 'dist/**', 'node_modules/**'],
+  ignoreFiles: ['dist/*', 'node_modules/**'],
   rules: {
     'comment-empty-line-before': null,
     'import-notation': null,
