39f45e16e23671ec53a44224e915c9610c9715dd
Max Milton <max@happyhackers.xyz>
2025-07-26 09:34:39 +0900

    chore: Move global types to separate .d.ts file

---
+++ globals.d.ts
@@ -0,0 +1,9 @@
+import type { ONCLICK } from "stage1/fast";
+
+declare global {
+  interface HTMLElement {
+    /** `stage1` synthetic click event handler. */
+    // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+    [ONCLICK]?(event: Event): false | void | Promise<void>;
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,4 @@
-import type { ONCLICK } from 'stage1/fast';
-import type { DEFAULT_SECTION_ORDER } from './utils.ts';
-
-declare global {
-  interface HTMLElement {
-    /** `stage1` synthetic click event handler. */
-    // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-    [ONCLICK]?(event: Event): false | void | Promise<void>;
-  }
-}
+import type { DEFAULT_SECTION_ORDER } from "./utils.ts";
 
 /** JSON object with theme name keys and raw CSS code values. */
 export type ThemesData = Record<string, string>;
