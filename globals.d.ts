27570a1bc7de2dc6e5fadbef61e322097f17f3c8
Max Milton <max@2okki.com>
2025-08-19 17:17:16 +0900

    chore: Adjust global type for correctness

--- globals.d.ts
+++ globals.d.ts
@@ -1,9 +1,9 @@
-import type { ONCLICK } from "stage1/fast";
+import { ONCLICK } from "stage1/fast";
 
 declare global {
   interface HTMLElement {
     /** `stage1` synthetic click event handler. */
     // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-    [ONCLICK]?(event: Event): false | void | Promise<void>;
+    [ONCLICK]?: (event: Event) => false | void | Promise<void>;
   }
 }
