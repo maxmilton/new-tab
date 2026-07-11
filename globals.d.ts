9ebeb0287ea9b7e2457c6f336b4a0fa42100d4cd
Max Milton <max@2okki.com>
2026-07-11 18:38:12 +1000

    chore: Stricter types

--- globals.d.ts
+++ globals.d.ts
@@ -1,6 +1,12 @@
 import type { ONCLICK } from "stage1/fast";
 
 declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly CI?: string;
+    }
+  }
+
   interface HTMLElement {
     /** `stage1` synthetic click event handler. */
     // oxlint-disable-next-line typescript/no-invalid-void-type
